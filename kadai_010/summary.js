e553b6a2b371d4bde3f854c6e0c0ce6ccacf2092
inada-ryuto <nyankoman1715@gmail.com>
2023-12-18 23:55:09 +0900

    kadai_010の機能追加

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -1,15 +1,36 @@
 $(function(){
+  //変数をリセット
+  let num = 0;
+  let text = 0;
 
   //文字色変更ボタンを押すと
   $('#change-color').on('click', () => {
+    //変数numが0のとき
+    if(num === 0){
     //id要素がtargetの色を赤に変える
     $('#target').css('color', 'red');
+    num = 1;
+    } 
+    //変数numが1のとき
+    else{
+      $('#target').css('color', 'black');
+      num = 0;
+    };
   });
 
   //文字内容変更ボタンを押すと
   $('#change-text').on('click', () => {
+    //変数textが1のとき
+    if (text === 0){
     //id要素がtargetの文字列をchangeに変える
     $('#target').text('change');
+    text = 1;
+    } 
+    //変数textが０のとき
+    else{ 
+      $('#target').text('こんにちは！');
+      text = 0;
+    };
   });
 
   //フェードアウトボタンを押すと
